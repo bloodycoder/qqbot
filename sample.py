c17a99cfa388ebe85408a0ebc3854024193c4c4f
bloodycoder <picardxie@foxmail.com>
2016-10-07 09:58:16 +0800

    dfs

--- sample.py
+++ sample.py
@@ -2,15 +2,16 @@
 # -*- coding: utf-8 -*-
 
 from qqbot import QQBot
-
+import time
 class MyQQBot(QQBot):
     def onPollComplete(self, msgType, from_uin, buddy_uin, message):
-        if message == '-hello':
-            self.send(msgType, from_uin, '你好，我是QQ机器人')
-        elif message == '-stop':
-            self.stopped = True
-            self.send(msgType, from_uin, 'QQ机器人已关闭')
-
+    	time.sleep(2)
+    	self.send(msgType,from_uin,self.getReturn(message))
+    def getReturn(self,message):
+    	resent = '这里是树莓派'
+    	if('hello' in message):
+    		resent = 'hello,how can I help you?'
+    	return resent
 myqqbot = MyQQBot()
 myqqbot.Login()
 myqqbot.PollForever()
